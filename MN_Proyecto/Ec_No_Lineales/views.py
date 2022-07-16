c5db64b2d010d8838e13224e2137ccd160de4ce4
Jose Cano <jcanov@unsa.edu.pe>
2022-07-16 17:22:08 -0500

    making the first view

--- MN_Proyecto/Ec_No_Lineales/views.py
+++ MN_Proyecto/Ec_No_Lineales/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'index.html')
+
+def metodoBiseccion(request):
+    if request.method == 'GET':
+        return render(request,'biseccion.html')
+    if request.method == 'POST':
+        nombre = request.POST['nombre']
+        return render(request,'biseccion.html',{'name':nombre})
